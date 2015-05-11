c70214a5bb02824c08965df599d23cdd3d2bf08b
pjc02478 <pjc0247@gmail.com>
2015-05-12 00:06:56 +0900

    Update timeunit.rb

--- src/timeunit.rb
+++ src/timeunit.rb
@@ -1,11 +1,9 @@
 module TimeUnit
   
-  class Second
-    def initialize s
-      @v = s
-    end
-    def to_s
-      return @v
-    end
+  def self.second v
+    return v
+  end
+  def self.millisecond v
+    return v.to_f / 1000
   end
 end
