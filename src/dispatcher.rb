bec5db6e360cbdfb8f0cd094cb43c1263fd28402
pjc02478 <pjc0247@gmail.com>
2015-05-12 00:01:04 +0900

    Create dispatcher.rb

---
+++ src/dispatcher.rb
@@ -0,0 +1,34 @@
+module Dispatcher
+  @running = Array.new
+  @pending = Array.new
+  
+  class Timer
+    attr_reader :sig
+    attr_accessor :remain, :last
+
+    def initialize timeunit
+      @sig = Flowcontrol::Signal.new
+      @remain = timeunit.to_s
+      @last = Time.now.to_f
+    end
+  end
+
+  def self.add_timer t
+    @pending.push t
+    return t
+  end
+
+  def self.step
+    puts "step"
+    @running.each do |t|
+       t.remain -= Time.now.to_f - t.last
+       t.last = Time.now.to_f
+        
+       if t.remain <= 0
+          @running.delete(t).sig.notify_one
+       end
+    end
+    
+    @running += @pending.pop(@pending.size)
+  end
+end
